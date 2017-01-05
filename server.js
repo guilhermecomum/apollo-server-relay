3e72b4c9dded2cfcd6bdeb678d93551ef855bcea
Guilherme Guerra <guerrinha@comum.org>
2017-01-05 15:30:45 -0200

    Setup basic webpack, react and relay

--- server.js
+++ server.js
@@ -6,44 +6,55 @@ import { createServer } from 'http';
 import { SubscriptionServer } from 'subscriptions-transport-ws';
 import { printSchema } from 'graphql/utilities/schemaPrinter';
 
-import { subscriptionManager } from './data/subscriptions';
+import path from 'path';
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackConfig from './config/webpack.config';
+import WebpackDevServer from 'webpack-dev-server';
+
 import schema from './data/schema';
 
-const GRAPHQL_PORT = 8080;
-const WS_PORT = 8090;
+const GRAPHQL_PORT = 3000;
+const app = express().use('*', cors());
 
-const graphQLServer = express().use('*', cors());
+const compiler = webpack(webpackConfig);
+const middleware = webpackDevMiddleware(compiler, {
+  noInfo: true,
+  publicPath: '/',
+  silent: true,
+  stats: 'errors-only',
+});
 
-graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({
+app.use('/graphql', bodyParser.json(), graphqlExpress({
   schema,
   context: {},
 }));
 
-graphQLServer.use('/graphiql', graphiqlExpress({
+app.use('/graphiql', graphiqlExpress({
   endpointURL: '/graphql',
 }));
 
-graphQLServer.use('/schema', (req, res) => {
+app.use('/schema', (req, res) => {
   res.set('Content-Type', 'text/plain');
   res.send(printSchema(schema));
 });
 
-graphQLServer.listen(GRAPHQL_PORT, () => console.log(
-  `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`
-));
+app.use(middleware);
+app.use(webpackHotMiddleware(compiler));
 
-// WebSocket server for subscriptions
-const websocketServer = createServer((request, response) => {
-  response.writeHead(404);
-  response.end();
+app.get('*', (req, res) => {
+  const indexFilePath = path.join('./app/', 'index.html');
+  middleware.fileSystem.readFile(indexFilePath, (err, file) => {
+    if (err) {
+      res.sendStatus(404);
+    } else {
+      res.send(file.toString());
+    }
+  });
 });
 
-websocketServer.listen(WS_PORT, () => console.log( // eslint-disable-line no-console
-  `Websocket Server is now running on http://localhost:${WS_PORT}`
-));
-
-// eslint-disable-next-line
-new SubscriptionServer(
-  { subscriptionManager },
-  websocketServer
-);
+app.listen(GRAPHQL_PORT, (err) => {
+  if (err) console.log(err);
+  console.log(`App is now running on http://localhost:${GRAPHQL_PORT}`);
+});
